5d6eb60c7fef4e6f41eb1f07c15af6bc7418a816
sorellelagrace <109281479+sorellelagrace@users.noreply.github.com>
2022-11-11 16:50:26 +0100

    1 file modified

--- src/Accueil.js
+++ src/Accueil.js
@@ -1,10 +1,21 @@
-import React from "react"
+import React, { useContext, useState, useEffect } from 'react';
 
-export default function Accueil(){
+export default function Accueil() {
+  const [storage, setStorage] = useState({});
 
-    return(
-      <div>
-        <h1>bonjour</h1>
-      </div>
-    )
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const appStorage = window.localStorage.getItem('appStorage');
+      const JsonEncoder = JSON.parse(appStorage);
+      console.log('encoder', JsonEncoder);
+      setStorage(JsonEncoder);
+    }
+  }, []);
+
+  console.log(storage);
+  return (
+    <div>
+      <h1>bonjour{storage.email}</h1>
+    </div>
+  );
 }
